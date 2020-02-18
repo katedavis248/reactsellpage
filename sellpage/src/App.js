0957de71d171f12aa39239626b3d77df72d0b3cb
Kate Davis <katedavis@kates-mbp.attlocal.net>
2020-02-18 13:52:43 -0600

    idk

--- sellpage/src/App.js
+++ sellpage/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 import { Nav, NavItem, NavLink } from 'reactstrap';
 
 function App() {
   return (
     <div>
-      <p>List Based</p>
+
       <Nav>
         <NavItem>
           <NavLink href="#">Link</NavLink>
@@ -22,12 +22,11 @@ function App() {
         </NavItem>
       </Nav>
       <hr />
-      <p>Link Based</p>
-      <Nav>
-        <NavLink href="#">Link</NavLink> <NavLink href="#">Link</NavLink> <NavLink href="#">Another Link</NavLink> <NavLink disabled href="#">Disabled Link</NavLink>
-      </Nav>
     </div>
   );
 }
+
+
+
 
 export default App;
