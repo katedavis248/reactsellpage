408df6071d84f07d16a6230743415c99150acc35
Kate Davis <katedavis@kates-mbp.attlocal.net>
2020-02-27 17:45:51 -0600

    changes

---
+++ sellpage/src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Loginpage from './Loginpage'
+import Navbar from './Navbar'
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar />
+            <Loginpage />
+        </div>
+
+    )
+}
+
+
---
+++ sellpage/src/pages/Navbar.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+
+const Navbar = (props) => {
+    const [collapsed, setCollapsed] = useState(true);
+
+    const toggleNavbar = () => setCollapsed(!collapsed);
+    return (
+        <div>
+            <Navbar color="faded" light>
+                <NavbarBrand href="/" className="mr-auto">Treasure</NavbarBrand>
+                <NavbarToggler onClick={toggleNavbar} className="mr-2" />
+                <Collapse isOpen={!collapsed} navbar>
+                    <Nav navbar>
+                        <NavItem>
+                            <NavLink href="/Home/">Home</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="/Cart/">Cart</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="/Login/">Login</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="/Sign Up/">Sign Up</NavLink>
+                        </NavItem>
+                    </Nav>
+                </Collapse>
+            </Navbar>
+        </div>
+    );
+}
+
+export default Navbar;
